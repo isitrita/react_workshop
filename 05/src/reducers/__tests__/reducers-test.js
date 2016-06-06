3dd60114b47e00cd91b7945194e88b49fa781944
is_it_rita <candor.cane@gmail.com>
2016-06-06 17:41:04 +0400

    Task 05. Initial commit. Normalizing the State Shape

---
+++ 05/src/reducers/__tests__/reducers-test.js
@@ -0,0 +1,33 @@
+import test from 'ava';
+import todos, { getVisibleTodos } from '../';
+
+test('top-level state object has `byId` and `allIds`', t => {
+  const state = todos(undefined, {});
+  t.true(state.hasOwnProperty('byId'));
+  t.true(state.hasOwnProperty('allIds'));
+});
+
+test('getVisibleTodos selects todos using given filter', t => {
+  const state = [
+    {},
+    { type: 'ADD_TODO', id: '1' },
+    { type: 'ADD_TODO', id: '2' },
+    { type: 'ADD_TODO', id: '3' },
+    { type: 'TOGGLE_TODO', id: '2' },
+  ].reduce(todos, undefined);
+
+  t.deepEqual(
+    getVisibleTodos(state, 'all').map(o => o.id),
+    ['1', '2', '3']
+  );
+
+  t.deepEqual(
+    getVisibleTodos(state, 'active').map(o => o.id),
+    ['1', '3']
+  );
+
+  t.deepEqual(
+    getVisibleTodos(state, 'completed').map(o => o.id),
+    ['2']
+  );
+});
