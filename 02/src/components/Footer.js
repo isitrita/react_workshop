fe303e72687ccf43369495f84c95183172b458f8
is_it_rita <candor.cane@gmail.com>
2016-06-06 16:17:36 +0400

    Removed unneeded comments

--- 02/src/components/Footer.js
+++ 02/src/components/Footer.js
@@ -1,21 +1,19 @@
 import React from 'react';
 import FilterLink from './FilterLink';
 //Replace constants like `'SHOW_ALL'` in `<Footer>` with paths like `'all'`.
+    //<FilterLink filter="SHOW_ACTIVE">
 const Footer = () => (
   <p>
     Show:
     {" "}
-    //<FilterLink filter="SHOW_ALL">
     <FilterLink filter="all">
       All
     </FilterLink>
     {", "}
-    //<FilterLink filter="SHOW_ACTIVE">
     <FilterLink filter="active">
       Active
     </FilterLink>
     {", "}
-    //<FilterLink filter="SHOW_COMPLETED">
     <FilterLink filter="completed">
       Completed
     </FilterLink>
